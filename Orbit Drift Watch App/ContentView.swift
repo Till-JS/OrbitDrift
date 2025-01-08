d1ae4e8fd37d22c762d4aeddf3024acadc65ab8d
Till-JS <101404291+Till-JS@users.noreply.github.com>
2025-01-08 03:09:23 +0100

    Initial Commit

---
+++ Orbit Drift Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Orbit Drift Watch App
+//
+//  Created by Till Schneider on 08.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Orbit Drift Watch App/Orbit_DriftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Orbit_DriftApp.swift
+//  Orbit Drift Watch App
+//
+//  Created by Till Schneider on 08.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct Orbit_Drift_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
